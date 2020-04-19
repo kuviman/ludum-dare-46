473fa6f77a953a0d849fd5cd5ad481f3d2ad5fc4
kuviman <kuviman@gmail.com>
2020-04-19 11:39:13 +0400

    Simple UI

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -4,24 +4,68 @@ use crate::*;
 pub struct Assets {}
 
 pub struct State {
+    theme: Rc<geng::ui::Theme>,
+    connection: geng::net::client::Connection<ServerMessage, ClientMessage>,
     princess_life: f64,
     princess_alive: bool,
+    button: geng::ui::TextButton,
 }
 
-impl Default for State {
-    fn default() -> Self {
+impl State {
+    fn new(
+        geng: &Rc<Geng>,
+        connection: geng::net::client::Connection<ServerMessage, ClientMessage>,
+    ) -> Self {
+        let theme = Rc::new(geng::ui::Theme {
+            color: Color::BLACK,
+            ..geng::ui::Theme::default(geng)
+        });
+        let theme = &theme;
         Self {
+            theme: theme.clone(),
+            connection,
             princess_life: 0.0,
             princess_alive: false,
+            button: geng::ui::TextButton::new(geng, theme, "FEED".to_owned(), 64.0),
         }
+    }
+    fn ui<'a>(&'a mut self) -> impl geng::ui::Widget + 'a {
+        use geng::ui::*;
+        let connection = &mut self.connection;
+        if self.princess_alive {
+            Box::new(geng::ui::column![
+                geng::ui::text(
+                    format!("Princess life: {}", self.princess_life),
+                    &self.theme.font,
+                    64.0,
+                    self.theme.color,
+                )
+                .align(vec2(0.5, 0.5))
+                .uniform_padding(16.0),
+                self.button
+                    .ui(Box::new(move || { connection.send(ClientMessage::Feed) }))
+                    .align(vec2(0.5, 0.5)),
+            ]) as Box<dyn geng::ui::Widget + 'a>
+        } else {
+            Box::new(
+                geng::ui::text(
+                    "Princess is dead :(",
+                    &self.theme.font,
+                    64.0,
+                    self.theme.color,
+                )
+                .align(vec2(0.5, 0.5)),
+            )
+        }
+        .align(vec2(0.5, 0.5))
     }
 }
 
 pub struct ClientApp {
     geng: Rc<Geng>,
     assets: Assets,
-    connection: geng::net::client::Connection<ServerMessage, ClientMessage>,
     state: State,
+    ui_controller: geng::ui::Controller,
 }
 
 impl ClientApp {
@@ -68,15 +112,15 @@ impl ClientApp {
         Self {
             geng: geng.clone(),
             assets,
-            connection,
-            state: default(),
+            state: State::new(geng, connection),
+            ui_controller: geng::ui::Controller::new(),
         }
     }
 }
 
 impl geng::State for ClientApp {
     fn update(&mut self, delta_time: f64) {
-        for message in self.connection.new_messages() {
+        for message in self.state.connection.new_messages() {
             match message {
                 ServerMessage::PrincessDied => {
                     info!("Princess died!");
@@ -89,37 +133,26 @@ impl geng::State for ClientApp {
                 _ => {}
             }
         }
+        self.ui_controller.update(self.state.ui(), delta_time);
     }
     fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
         ugli::clear(framebuffer, Some(Color::WHITE), None);
         let framebuffer_size = framebuffer.size().map(|x| x as f32);
-        if self.state.princess_alive {
-            self.geng.default_font().draw_aligned(
-                framebuffer,
-                &format!("Princess life: {}", self.state.princess_life.max(0.0)),
-                vec2(framebuffer_size.x / 2.0, framebuffer_size.y / 2.0),
-                0.5,
-                32.0,
-                Color::BLACK,
-            );
-        } else {
-            self.geng.default_font().draw_aligned(
-                framebuffer,
-                "Princess is dead :(",
-                vec2(framebuffer_size.x / 2.0, framebuffer_size.y / 2.0),
-                0.5,
-                32.0,
-                Color::BLACK,
-            );
-        }
+        self.ui_controller.draw(self.state.ui(), framebuffer);
     }
     fn handle_event(&mut self, event: geng::Event) {
+        if self
+            .ui_controller
+            .handle_event(self.state.ui(), event.clone())
+        {
+            return;
+        }
         match event {
             geng::Event::KeyDown {
                 key: geng::Key::Space,
                 ..
             } => {
-                self.connection.send(ClientMessage::Feed);
+                self.state.connection.send(ClientMessage::Feed);
             }
             _ => {}
         }
